137813d81da7e5c566a457d42a3fbe769d422d6d
Lauty13 <lautaroguajardo4@gmail.com>
2023-06-12 21:36:37 -0300

    Archivo subido por primera vez

---
+++ floreria_app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Cliente, DetalleProducto, DetalleVenta, EstadoVenta, Insumo, Producto, Sucursal, Venta
+
+# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(DetalleProducto)
+admin.site.register(DetalleVenta)
+admin.site.register(EstadoVenta)
+admin.site.register(Insumo)
+admin.site.register(Producto)
+admin.site.register(Sucursal)
+admin.site.register(Venta)
+
+# Register your models here.
